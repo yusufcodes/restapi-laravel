0cec1a824f00b6a011ec71009107459f354aafee
yusufcodes <yusufcodes@outlook.com>
2019-09-17 19:12:56 +0100

    Removed unneeded controllers

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -17,16 +17,6 @@ class MeetingController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return 'Create (GET): Working!';
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -46,17 +36,6 @@ class MeetingController extends Controller
     public function show($id)
     {
         return 'Show (GET): Working!';
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        return 'Edit (GET): Working!';
     }
 
     /**
