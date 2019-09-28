123246c4851341887407aed6d274b7327fc2a2f1
yusufcodes <yusufcodes@outlook.com>
2019-09-28 12:46:45 +0100

    Amended AuthController to allow for a user to be created and added to the database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,38 +2,56 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\User;
 
 class AuthController extends Controller
 {
     public function store(Request $request)
     {
+        // Validating the input by the user
         $this->validate($request, [
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required|min:5'
         ]);
 
+        // Retrieving the user's input
         $name = $request->input('name');
         $email = $request->input('email');
         $password = $request->input('password');
 
-        $user = [
+        // Creating a new User model
+        $user = new User([
             'name' => $name,
             'email' => $email,
-            'password' => $password,
-            'signin' => [
+            'password' => bcrypt($password)
+            ]);
+
+        // Actions to perform if storing the user was successful
+        if ($user->save())
+        {
+            // Add a property 'signin' to the User model
+            $user->signin = [
                 'href' => 'api/v1/user/signin',
                 'method' => 'POST',
                 'params' => 'email, password'
-            ]
-        ];
+            ];
 
+            // Creating and returning a json response back
+            $response = [
+                'msg' => 'User created',
+                'user' => $user
+            ];
+
+            return response()->json($response, 201);
+        }
+
+        // Actions to perform if creating a user was not successful
         $response = [
-            'msg' => 'User created',
-            'user' => $user
+            'msg' => 'Error in creating user'
         ];
 
-        return response()->json($response, 201);
+        return response()->json($response, 404);
     }
 
     public function signin(Request $request)
